a3b5db8b03124f070e8ecf5dbc8413360fd14da9
yufu.deng <observeralone@gmail.com>
2022-04-08 17:36:28 +0800

    test

--- mock/zipkin-mock/src/main/java/com/megaease/easeagent/mock/zipkin/MockTracingProvider.java
+++ mock/zipkin-mock/src/main/java/com/megaease/easeagent/mock/zipkin/MockTracingProvider.java
@@ -17,8 +17,11 @@
 
 package com.megaease.easeagent.mock.zipkin;
 
+import brave.Span;
 import brave.TracerTestUtils;
 import brave.Tracing;
+import brave.propagation.CurrentTraceContext;
+import brave.propagation.ThreadLocalCurrentTraceContext;
 import com.megaease.easeagent.mock.config.MockConfig;
 import com.megaease.easeagent.mock.report.MockReport;
 import com.megaease.easeagent.mock.utils.MockProvider;
@@ -50,5 +53,12 @@ public class MockTracingProvider implements MockProvider {
 
     public static synchronized void cleanPendingSpans() {
         TracerTestUtils.clean(TRACING.tracer());
+    }
+
+    public static synchronized void cleanCurrentSpan() {
+        CurrentTraceContext currentContext = TRACING.currentTraceContext();
+        if (currentContext instanceof ThreadLocalCurrentTraceContext) {
+            ((ThreadLocalCurrentTraceContext) currentContext).clear();
+        }
     }
 }
